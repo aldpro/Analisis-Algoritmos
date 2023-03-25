b33d5c62a047984ef6ffea54fb50fd5c35185935
cristian <cristians.barreram@uqvirtual.edu.co>
2023-03-25 15:46:24 -0500

    Optimizado el main para mostrar resultados

--- src/co/edu/analisis/Aplicacion.java
+++ src/co/edu/analisis/Aplicacion.java
@@ -8,47 +8,10 @@ public class Aplicacion {
 	
 	public static void main(String[] args) {
 		
-		Captura captura = new Captura();
-		
 		int[][] matrizn = {{1234, 2345, 3456, 4567}, {5678, 6789, 7890, 8901}, {9012, 1234, 2345, 3456}, {4567, 5678, 6789, 7890}};
 		int[][] matrizm = {{4321, 5432, 6543, 7654}, {8765, 9876, 1098, 2109}, {3210, 4321, 5432, 6543}, {7654, 8765, 9876, 1098}};
 
-		Object[] resultado = captura.capturaNaivStandard(matrizn, matrizm);
-		
-		long tiempo = (long) resultado[0];
-		int[][] matrizNaivStandard = (int[][]) resultado[1];
-		
-		System.out.println("Matriz {A}:");
-		
-		for (int i = 0; i < matrizn.length; i++) {
-			  for (int j = 0; j < matrizn[i].length; j++) {
-			    System.out.print(matrizn[i][j] + " ");
-			  }
-			  System.out.println();
-		}
-		
-		System.out.println("\n" + "Matriz {B}:");
-		
-		for (int i = 0; i < matrizm.length; i++) {
-			  for (int j = 0; j < matrizm[i].length; j++) {
-			    System.out.print(matrizm[i][j] + " ");
-			  }
-			  System.out.println();
-		}
-		
-		System.out.println("\n" + "Matriz {C}:");
-		
-		int digitos = (int) Math.log10(arregloMatriz(matrizNaivStandard)) + 1;
-		
-		for (int i = 0; i < matrizNaivStandard.length; i++) {
-		   for (int j = 0; j < matrizNaivStandard[i].length; j++) {
-		     
-		     System.out.print(String.format("%" + digitos + "d" + " ",matrizNaivStandard[i][j]));
-		   }
-		   System.out.println();
-		}
-		
-		System.out.println("Tiempo: " + tiempo + " [milisegundos]" + "\n" + "Algoritmo: NaivStandard");
+		resultado(matrizn, matrizm);
 	}
 	
 	public static int arregloMatriz(int[][] matriz) {
@@ -61,5 +24,35 @@ public class Aplicacion {
 		   }
 		}
 		return maximo;
+	}
+	
+	public static void imprimirMatriz (int[][] matriz, String nombre) {
+		int digitos = (int) Math.log10(arregloMatriz(matriz)) + 1;
+		
+		System.out.println("Matriz [" + nombre + "]:");
+		
+		for (int i = 0; i < matriz.length; i++) {
+		   for (int j = 0; j < matriz[i].length; j++) {
+		     
+		     System.out.print(String.format("%" + digitos + "d" + " ",matriz[i][j]));
+		   }
+		   System.out.println();
+		}
+		 System.out.println();
+	}
+	
+	public static void resultado (int[][] matrizn, int[][] matrizm) {
+		Captura captura = new Captura();
+		
+		Object[] resultado = captura.capturaNaivStandard(matrizn, matrizm);
+		
+		long tiempo = (long) resultado[0];
+		int[][] matrizResultado = (int[][]) resultado[1];
+		
+		imprimirMatriz(matrizn, "A");
+		imprimirMatriz(matrizm, "B");
+		imprimirMatriz(matrizResultado, "C");
+		
+		System.out.println("Tiempo: " + tiempo + " [milisegundos]" + "\n" + "Algoritmo: NaivStandard");
 	}
 }
