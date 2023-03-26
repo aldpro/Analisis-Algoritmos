8cb213121596c09e335a99d1d51b123561f0c157
Alejandro Barragan <93490270+aldpro@users.noreply.github.com>
2023-03-26 16:54:30 -0500

    .

--- src/co/edu/analisis/A.java
+++ src/co/edu/analisis/A.java
@@ -2,6 +2,7 @@ package co.edu.analisis;
 
 import java.util.Arrays;
 
+import co.edu.analisis.model.methods.NaivLoopUnrollingFour;
 import co.edu.analisis.model.methods.WinogradScaled;
 
 public class A {
@@ -9,12 +10,12 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		WinogradScaled a = new WinogradScaled();
+		NaivLoopUnrollingFour a = new NaivLoopUnrollingFour();
 		
 		double[][] matrizn = {{1234, 2345, 3456, 4567}, {5678, 6789, 7890, 8901}, {9012, 1234, 2345, 3456}, {4567, 5678, 6789, 7890}};
 		double[][] matrizm = {{4321, 5432, 6543, 7654}, {8765, 9876, 1098, 2109}, {3210, 4321, 5432, 6543}, {7654, 8765, 9876, 1098}};
 		
-		double[][] sd = a.winogradScaled(matrizn, matrizm);
+		double[][] sd = a.naiveLoopUnrollingFour(matrizn, matrizm);
 		
 		for (int i = 0; i < sd.length; i++) {
 		    for (int j = 0; j < sd[i].length; j++) {
